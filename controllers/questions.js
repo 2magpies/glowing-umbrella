db6a48891e61b054b80a84bc55d41b80d830919a
tiffhuddleston <tiffhuddleston@gmail.com>
2020-02-06 15:22:07 -0600

    added views and routes

--- controllers/questions.js
+++ controllers/questions.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Question = require('../models/Question');
-
 const router = express.Router();
 
 router.get('/', (req, res) => {
@@ -9,6 +8,16 @@ router.get('/', (req, res) => {
       res.render('index', { questions });
     })
     .catch(console.error);
+});
+
+router.get('/new', (req, res) => {
+  res.render('new');
+});
+
+router.get('/:id/answer', (req, res) => {
+  Question.findById(req.params.id).then(question => {
+    res.render('answer', question);
+  });
 });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const questionsController = require('./controllers/questions');
 
 const express = require('express');
 const app = express();
+const methodOverride = require('method-override');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'hbs');
-
-const methodOverride = require('method-override');
 
 app.use(methodOverride('_method'));
 
