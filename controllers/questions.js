e5105914ef92779dbe3193009ff2f3167035d01b
2magpies <daphnemintz@yahoo.com>
2020-02-06 17:26:09 -0500

    Completed MVP

--- controllers/questions.js
+++ controllers/questions.js
@@ -20,4 +20,39 @@ router.get('/:id/answer', (req, res) => {
   });
 });
 
+router.get('/:id', (req, res) => {
+  Question.findOne({ _id: req.params.id })
+    .then(question => {
+      res.render('show', question);
+    })
+    .catch(console.error);
+});
+
+router.post('/', (req, res) => {
+  Question.create(req.body)
+    .then(question => {
+      res.redirect('/questions');
+    })
+    .catch(console.error);
+});
+
+router.put('/:id', (req, res) => {
+  console.log(req);
+  const question = {
+    answer: req.body.answer
+  };
+  Question.findOneAndUpdate({ _id: req.params.id }, question, {
+    new: true
+  }).then(question => {
+    console.log(question);
+    res.redirect('/questions');
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  Question.findOneAndRemove({ _id: req.params.id }).then(() => {
+    res.redirect('/questions');
+  });
+});
+
 module.exports = router;
